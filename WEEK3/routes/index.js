db0a3d05f01ad81f1f6b5fcbadc201e2bdcdcc79
HimchanBae <oiu3056@naver.com>
2024-08-09 17:58:07 +1000

    WEEK3 - Lecture (MongoDB Set up)

--- WEEK3/routes/index.js
+++ WEEK3/routes/index.js
@@ -1,10 +1,55 @@
+url =
+  "mongodb+srv://himchanbae:Ehfkdpahd94!@cluster0.b0rbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+
+let userInformation = [
+  { name: "michael", value: 50 },
+  { name: "phill", value: 80 },
+];
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+const userSchema = new mongoose.Schema({ user: String, info: String });
+
+const UserInfo = mongoose.model("user", userSchema);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  req.session.sessionid = "something";
-  res.render("index", { title: "Express", sessionid: req.session.sessionid });
+  //req.session.sessionid = "something";
+
+  UserInfo.find({}).then((result) => {
+    res.render("index", {
+      title: "Express",
+      sessionid: req.session.sessionid,
+      userInformation: result,
+    });
+  });
+
+  console.log("I reached here");
+});
+
+router.post("/", function (req, res) {
+  console.log(req.body.cookievalue);
+  req.session.sessionid = req.body.cookievalue;
+  res.redirect("/");
+});
+
+router.post("/UserInformation", function (req, res) {
+  // console.log(req.body.value, req.body.value);
+  // userInformation.push({ name: req.body.value, value: req.body.value });
+
+  const userInfo = new UserInfo({
+    user: req.body.user,
+    info: req.body.value,
+  });
+
+  userInfo.save();
+
+  res.redirect("/");
 });
 
 module.exports = router;
